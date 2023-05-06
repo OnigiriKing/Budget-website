950cf31df0ccba32737bbe4b18ef93f65197589f
OnigiriKing <112744729+OnigiriKing@users.noreply.github.com>
2023-05-06 16:46:47 +0700

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-let type = event.target.parentNode.id.split("-");
-let id = type[1];
-index = data.allItems[type].findIndex((obj) => obj.id === id);
-data.total.income -= data.allItems[type][index].val;
-data.allItems[type].splice(index, 1);
-document.getElementById(`${event.target.parentNode.parentNode.id}`).remove();
-updateHTML();
